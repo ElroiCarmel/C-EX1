7a196bdec3c8c78c8ecd5e5d398d2ddbf6f0b682
elroi carmel <elroica277@gmail.com>
2024-01-24 11:19:29 +0200

    no need for IFNDEF

--- numClass.h
+++ numClass.h
@@ -1,9 +1,4 @@
-//
-// Created by parallels on 1/18/24.
-//
 
-#ifndef C_EX1_NUMCLASS_H
-#define C_EX1_NUMCLASS_H
 int isPrime(int);
 int isStrong(int);
 int isArmstrong(int);
@@ -13,4 +8,4 @@ int power(int, int);
 int factorial(int);
 int isPalindromeHelper(int* , int, int);
 int getArmstrongNumber(int, int);
-#endif //C_EX1_NUMCLASS_H
+
