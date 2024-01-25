9351bdbd4964b9e583f33cf9944282e9e454fd9b
elroi carmel <elroica277@gmail.com>
2024-01-25 20:35:14 +0200

    making recursion more recursive

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -24,21 +24,20 @@ int isPalindromeHelper(int* num, int i, int j)
     return 0;
 }
 
-int getArmstrongNumber(int num, int len)
+int isArmstrongHelper(int sumLeft, int numWithoutLastDigit, int len)
 {
-    if (num == 0)
-    {
-        return 0;
-    }
-    return power(num % 10, len) + getArmstrongNumber(num / 10, len);
+    if (sumLeft < 0) return 0;
+    if (numWithoutLastDigit == 0) return sumLeft==0;
+    int ldp = power(numWithoutLastDigit%10, len);
+    return isArmstrongHelper(sumLeft-ldp, numWithoutLastDigit/10, len);
 }
 
 int isArmstrong(int num)
 {
     if (num < 0) return 0;
     int nd = numDigits(num);
-    int armNum = getArmstrongNumber(num, nd);
-    return num == armNum;
+    int ans = isArmstrongHelper(num, num, nd);
+    return ans;
 }
 
 
